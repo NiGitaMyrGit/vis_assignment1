90ac9080416d1234c94af6c1b5d0228c589fb29c
NiGitaMyrGit <201906799@post.au.dk>
2023-03-05 00:35:43 +0100

    playing around with bin coun

--- src/histograms_comparison.py
+++ src/histograms_comparison.py
@@ -30,9 +30,7 @@ for filename in os.listdir(flower_dir):
     hist = cv2.calcHist([image], [0, 1, 2], None, [256, 256, 256], [0, 256, 0, 256, 0, 256])
     
     # Calculate the distance between the target histogram and the image histogram
-    # using the Bhattacharyya distance
     # using Bhattacharyya distance, in order to measure the “overlap” between the two histograms:
-    # https://pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
     distance= cv2.compareHist(target_hist, hist, cv2.HISTCMP_BHATTACHARYYA)
     
     # Add the filename and distance to the distances list
@@ -44,7 +42,7 @@ distances.sort(key=lambda x: x[1])
 # Create a list to store the top 5 images
 top_5 = []
 
-# Add the target image to the top 5 list with a distance of 0.0
+# Add the target_image to the top 5 list with a distance of 0.0 (since it's the same)
 top_5.append((os.path.basename(target_filename), 0.0))
 
 # Loop over the 5 images with the smallest distance and add them to the top 5 list
@@ -58,3 +56,20 @@ with open("similar_images.csv", "w", newline="") as csvfile:
     writer.writerow(["Filename", "Distance"])
     for item in top_5:
         writer.writerow(item)
+
+''' I have tried meddling with the bin count. 
+One of the datatables in the outfolder showcases the results
+with an adjusted bin-count of 36 bins instead of the 256
+I must admit I with my human eyes at least, cannot really tell apart how similiar each image is or not.
+A promising thing is that the exact same picture (image_) '''
+
+'''Furthermore, I wish to understand the functions, I'll get there:
+took me a while to understand what was even meant by making functions xD'''
+
+# Help sources:
+# Bhattacharyya distance:
+# https://pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
+# fnmatch: https://docs.python.org/3/library/fnmatch.html
+# something about lambda functions and very basic lambda calculus:
+# https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
+# https://www.youtube.com/watch?v=d0yEXKas8xE
